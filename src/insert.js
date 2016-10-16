04b9cc52ef68e62fcfdb8c06c38b7761254bbe16
azu <azuciao@gmail.com>
2016-10-16 21:16:01 +0900

    fix(module): module.exports

--- src/insert.js
+++ src/insert.js
@@ -1,8 +1,8 @@
 const remark = require("remark")();
 const inject = require('mdast-util-inject');
-export default function insert(content, insertContent, sectionName) {
+module.exports = function insert(content, insertContent, sectionName, remarkOptions = {}) {
     const target = remark.parse(content);
     const newStuff = remark.parse(insertContent);
     inject(sectionName, target, newStuff);
-    return remark.stringify(target)
-}
+    return remark.stringify(target, remarkOptions)
+};
